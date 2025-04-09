3ba2cc64a6617bce5e82ad0c76d6d80adcf00454
Manrick-Lazaro <manricksantos@gmail.com>
2025-04-09 13:39:16 -0300

    feat: implements layout of search input.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -50,6 +50,70 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Expanded(
+          child: Row(
+            children: [
+              Image.network(
+                'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWM5cHl1eW1mbmQ5Y3NxMGRwZ2VjNDNxa21sa2s1NzBrb2FhMmI4ZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7buevvUs7Y9Bw9lm/giphy.gif',
+                width: 50,
+                height: 50,
+              ),
+              Text('IGiphys', style: TextStyle(color: Colors.white)),
+            ],
+          ),
+        ),
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: 'Search all the GIFs',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          topLeft: Radius.circular(10),
+                        ),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.purple, Colors.pinkAccent],
+                      begin: Alignment.bottomLeft,
+                      end: Alignment.topRight,
+                    ),
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                  ),
+                  padding: EdgeInsets.only(left: 5, right: 5, bottom: 3, top: 2.5),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.search),
+                    color: Colors.white,
+                    iconSize: 35,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
