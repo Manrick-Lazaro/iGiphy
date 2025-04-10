f37c8e1a41dc04aed0599205e65422d22bc77c16
Manrick-Lazaro <manricksantos@gmail.com>
2025-04-10 12:01:36 -0300

    feat: enables the search field.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
@@ -11,7 +12,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  String? _search;
+  final TextEditingController _searchController = TextEditingController();
+  String _search = '';
   int _offset = 0;
   final _apiKey = 'b5ln3ER46WbOOjCeEaC0zpA0cJ8CTalA';
 
@@ -26,7 +28,7 @@ class _HomeState extends State<Home> {
       'q': _search,
     };
 
-    if (_search == null) {
+    if (_search == '') {
       route = '/v1/gifs/trending';
 
       params.remove('q');
@@ -37,15 +39,6 @@ class _HomeState extends State<Home> {
     response = await http.get(url);
 
     return json.decode(response.body);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    _getGiphys().then((map) {
-      print('========>>>>>>>  $map');
-    });
   }
 
   @override
@@ -67,106 +60,148 @@ class _HomeState extends State<Home> {
         ),
       ),
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintText: 'Search all the GIFs',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(10),
-                          topLeft: Radius.circular(10),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Search all the GIFs',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10),
+                            topLeft: Radius.circular(10),
+                          ),
+                          borderSide: BorderSide.none,
                         ),
-                        borderSide: BorderSide.none,
+                      ),
+                      onSubmitted: (text) {
+                        setState(() {
+                          _search = text;
+                          _offset = 0;
+                        });
+                      },
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Colors.purple, Colors.pinkAccent],
+                        begin: Alignment.bottomLeft,
+                        end: Alignment.topRight,
+                      ),
+                      borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(10),
+                        topRight: Radius.circular(10),
                       ),
                     ),
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Colors.purple, Colors.pinkAccent],
-                      begin: Alignment.bottomLeft,
-                      end: Alignment.topRight,
+                    padding: EdgeInsets.only(
+                      left: 5,
+                      right: 5,
+                      bottom: 3,
+                      top: 2.5,
                     ),
-                    borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(10),
-                      topRight: Radius.circular(10),
+                    child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _search = _searchController.text;
+                          _offset = 0;
+                        });
+                      },
+                      icon: const Icon(Icons.search),
+                      color: Colors.white,
+                      iconSize: 35,
                     ),
                   ),
-                  padding: EdgeInsets.only(
-                    left: 5,
-                    right: 5,
-                    bottom: 3,
-                    top: 2.5,
-                  ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.search),
-                    color: Colors.white,
-                    iconSize: 35,
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: FutureBuilder(
-              future: _getGiphys(),
-              builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting:
-                  case ConnectionState.none:
-                    return Container(
-                      alignment: Alignment.center,
-                      width: 200,
-                      height: 200,
-                      child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                        strokeWidth: 5,
-                      ),
-                    );
-                  default:
-                    if (snapshot.hasError) {
-                      return Container();
-                    } else {
-                      return _createGiphysTable(context, snapshot);
-                    }
-                }
-              },
+            Expanded(
+              child: FutureBuilder(
+                future: _getGiphys(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                      return Container(
+                        alignment: Alignment.center,
+                        width: 200,
+                        height: 200,
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                            Colors.white,
+                          ),
+                          strokeWidth: 5,
+                        ),
+                      );
+                    default:
+                      if (snapshot.hasError) {
+                        return Container();
+                      } else {
+                        return _createGiphysTable(context, snapshot);
+                      }
+                  }
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
   Widget _createGiphysTable(BuildContext context, AsyncSnapshot snapshot) {
-    return GridView.builder(
-      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-      ),
-      itemCount: snapshot.data['data'].length,
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          child: Image.network(
-            snapshot.data['data'][index]['images']['downsized']['url'],
-            height: 300,
-            fit: BoxFit.cover,
-            gaplessPlayback: true,
+    return Column(
+      children: [
+        Expanded(
+          child: GridView.builder(
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemCount: snapshot.data['data'].length,
+            itemBuilder: (context, index) {
+              if (index < snapshot.data['data'].length - 1) {
+                return GestureDetector(
+                  child: Image.network(
+                    snapshot.data['data'][index]['images']['downsized']['url'],
+                    height: 300,
+                    fit: BoxFit.cover,
+                    gaplessPlayback: true,
+                  ),
+                );
+              } else {
+                return GestureDetector(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.add, color: Colors.white, size: 70),
+                      Text(
+                        'Carregar mais...',
+                        style: TextStyle(color: Colors.white, fontSize: 22),
+                      ),
+                    ],
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _offset += 24;
+                    });
+                  },
+                );
+              }
+            },
           ),
-        );
-      },
+        ),
+      ],
     );
   }
 }
