8f20457b1d64d1ee3508a0ece0c444726b7afa61
Manrick-Lazaro <manricksantos@gmail.com>
2025-04-10 09:18:21 -0300

    feat: implements class myApp in main file.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,16 @@ import 'package:flutter/material.dart';
 import 'ui/home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
 }
